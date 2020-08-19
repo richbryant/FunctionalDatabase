3540412da0dcc6acd86657cd82dc69e8ff460c4f
Rich Bryant <rich.bryant@outlook.com>
2020-08-20 00:17:41 +0100

    Mappers

---
+++ FunctionalDatabase/Shared/MappingProfiles/NorthwindProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using FunctionalDatabase.Shared.DataTransferObjects;
+using FunctionalDatabase.Shared.Models;
+
+namespace FunctionalDatabase.Shared.MappingProfiles
+{
+    public class NorthwindProfile : Profile
+    {
+        public NorthwindProfile()
+        {
+            CreateMap<Category, CategoryDto>();
+            CreateMap<Customer, CustomerDto>();
+            CreateMap<Employee, EmployeeDto>();
+            CreateMap<OrderDetail, OrderDetailsDto>();
+            CreateMap<Order, OrderDto>();
+            CreateMap<Product, ProductDto>();
+            CreateMap<Region, RegionDto>();
+            CreateMap<Shipper, ShipperDto>();
+            CreateMap<Supplier, SupplierDto>();
+            CreateMap<Territory, TerritoryDto>();
+        }
+    }
+}
